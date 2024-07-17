fc6a6b165257eb7c4690d7b9bce4b43c9c2287e3
m5379mm <2536044181@qq.com>
2024-07-18 02:06:31 +0800

    更新

--- user/sleep.c
+++ user/sleep.c
@@ -1,7 +1,8 @@
 #include "kernel/types.h"
 #include "user/user.h"
 
-// argc:命令行参数的数量，argv：只想字符串数组的指针，每个字符串都是一个命令行参数
+// argc:命令行参数的数量
+// argv：指向字符串数组的指针，每个字符串都是一个命令行参数
 int 
 main(int argc,char *argv[])
 {
